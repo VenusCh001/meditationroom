771a001936c16d5662f9f79cbe3841fc444709ab
Glitch (aframe-360-video-example) <none>
2019-04-23 02:37:12 +0000

    🐜🚨 Checkpoint ./arrow-key-rotation.js:3739039/495

--- arrow-key-rotation.js
+++ arrow-key-rotation.js
@@ -1,6 +1,6 @@
 
 var index = 0;
-var videoSrc = ["src: #video0;", "src: #video1;", "src: #video2;"];
+var videoSrc = ["src: #video0", "src: #video1", "src: #video2"];
 var videos = ["#video0", "#video1", "#video2"];
 var audios = ["#audio0", "#audio1", "#audio2"];
 
@@ -24,8 +24,9 @@ function playAudio(){
 function pauseAudio(){
     var audioEl = document.querySelector("#voice");
     audioEl.setAttribute("src", audios[index]);
-
+    
     var audio = audioEl.components.sound;
+    console.log(audio);
     audio.stopSound();
 
 }
@@ -35,7 +36,6 @@ function pauseVideo(){
   var videoTextNow = document.querySelector('a-videosphere').getAttribute('src');
 
   var video = document.querySelector(videoTextNow.substring(NaN,videoTextNow.length));
-  console.log(videoTextNow.substring(NaN,videoTextNow.length));
   video.pause();
 }
 
@@ -55,8 +55,10 @@ function start(i){
 function exit(){
   index = 0;
   pauseVideo();
-  document.querySelector("#voice").play();
+  pauseAudio();
   document.querySelector("#sound").play();
+  var audioEl = document.querySelector("#voice");
+  audioEl.setAttribute("src", "#voiceover");
   document.getElementById("videoStuff").setAttribute('visible','false');
   document.getElementById("mainStuff").setAttribute('visible','true');
   document.querySelector('a-videosphere').setAttribute('material', "src: #video0;");
